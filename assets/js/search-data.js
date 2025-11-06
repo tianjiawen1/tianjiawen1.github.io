73238a010f3471796554feb6cbbaa683c9dbaa30
tianjiawen1 <97448664+tianjiawen1@users.noreply.github.com>
2025-11-06 03:42:29 +0000

    Deploying to gh-pages from @ tianjiawen1/tianjiawen1.github.io@19dc4f82d0754e15843432c23a59c1083015a452 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,7 +46,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "All about me！",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
