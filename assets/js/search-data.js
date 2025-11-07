4524fa9e24297e1b9455f9d2b2dae4021ab297eb
tianjiawen1 <97448664+tianjiawen1@users.noreply.github.com>
2025-11-07 10:06:35 +0000

    Deploying to gh-pages from @ tianjiawen1/tianjiawen1.github.io@5a57beebdd95aff5a016962d49b1270406e33f59 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -115,6 +115,11 @@ ninja.data = [{
           description: "a lightweight wheeled humanoid dual-arm robot",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-e-umi",
+          title: 'E-UMI',
+          description: "data acquisition device",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{
         id: 'social-email',
         title: 'email',
