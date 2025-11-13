520bb8f86b9cb069eac8ad3d37b6b1e0238c36be
tianjiawen1 <97448664+tianjiawen1@users.noreply.github.com>
2025-11-13 01:45:17 +0000

    Deploying to gh-pages from @ tianjiawen1/tianjiawen1.github.io@4228cd44c914885ee3e36b168c5d421561a99387 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -110,16 +110,6 @@ ninja.data = [{
           description: "PhD research project",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-bytemini",
-          title: 'ByteMini',
-          description: "a lightweight wheeled humanoid dual-arm robot",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-e-umi",
-          title: 'E-UMI',
-          description: "data acquisition device",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
             },},{
         id: 'social-email',
         title: 'email',
